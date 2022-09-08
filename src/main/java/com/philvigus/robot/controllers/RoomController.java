f2b8f4d07809a26de17c396dca34314eb96fd211
PhilipVigus <phil.vigus@devoteam.com>
2022-09-08 16:22:41 +0200

    Create a RoomController

---
+++ src/main/java/com/philvigus/robot/controllers/RoomController.java
@@ -0,0 +1,29 @@
+package com.philvigus.robot.controllers;
+
+import com.philvigus.robot.domain.Room;
+import com.philvigus.robot.services.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rooms")
+public class RoomController {
+    RoomService roomService;
+
+    @Autowired
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Room>> getRooms() {
+        return new ResponseEntity<>(roomService.findAll(), new HttpHeaders(), HttpStatus.OK);
+    }
+}
